be7684f67b338d019cf541881534277ee1e408cd
Kayanequeiroz <meusrecibos2022+kayane@gmail.com>
2024-08-01 13:09:46 -0300

    aidiciona uma view e url para o meu template

--- atividade/urls.py
+++ atividade/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
+from atividade import views
 
 urlpatterns = [
-    
+    path('kayane/', views.view_kayane, name='kayane')
 ]
--- atividade/views.py
+++ atividade/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def view_kayane(request):
+    nome = 'kayane'
+    return render(request, 'atividade/template-kayane.html',
+                  context={'nome': nome}
+                  )
