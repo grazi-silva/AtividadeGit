764a9847fc1e21a3905c3a9acd14f0dffa07fc46
grazi-silva <meusrecibos2022+grazi@gmail.com>
2024-08-01 13:00:56 -0300

    cria uma url e view para acessar o meu template

--- atividade/urls.py
+++ atividade/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
+from atividade import views
 
 urlpatterns = [
-    
+    path('grazi/', views.view_grazi, name='view_grazi')
 ]
--- atividade/views.py
+++ atividade/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def view_grazi(request):
+    nome = 'Grazi'
+    return render (request, 'atividade/template-grazi.html', 
+                   context= {'nome': nome}
+                   )
